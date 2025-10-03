e707be679017e49c72ede5408f692c5cc55a4f36
Vanshika-30 <jvanshika30@gmail.com>
2025-10-03 13:31:06 -0500

    changed CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link, useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 import { MediaData } from "./api/nasaTypes";
